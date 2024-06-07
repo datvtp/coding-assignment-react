b79a147b85d27ad16208f8d1a25ec939b8379e96
dat <vanthanhphuongdat1309@gmail.com>
2024-06-07 19:23:11 +0700

    ticket details

--- client/src/app/ticket-details/ticket-details.tsx
+++ client/src/app/ticket-details/ticket-details.tsx
@@ -1,14 +1,58 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { x } from '@xstyled/styled-components';
+import { Box } from '@welcome-ui/box';
+import { Text } from '@welcome-ui/text';
+
+import { Ticket } from '@acme/shared-models';
 
 export function TicketDetails() {
+  const [ticket, setTicket] = useState<Ticket>();
   const { id } = useParams();
 
+  useEffect(() => {
+    if (!id) return;
+
+    async function fetchTickets() {
+      const data = await fetch(`/api/tickets/${id}`).then();
+      setTicket(await data.json());
+    }
+
+    fetchTickets();
+  }, [id]);
+
+  if (!ticket) {
+    return null;
+  }
+
   return (
-    <x.div>
-      <x.h1>Welcome to Ticket: {id}</x.h1>
-    </x.div>
+    <Box
+      maxW={480}
+      display="flex"
+      flexDirection="column"
+      mx="auto"
+      borderStyle="solid"
+      borderWidth="md"
+      borderColor="primary-500"
+      borderRadius="lg"
+      p="md"
+    >
+      <Text variant="h2" color="primary-700" textAlign="center">
+        Ticket Details
+      </Text>
+      <Text>
+        <b>ID:</b> {id}
+      </Text>
+      <Text>
+        <b>Description:</b> {ticket.description}
+      </Text>
+      <Text>
+        <b>Assignee ID:</b> {ticket.assigneeId}
+      </Text>
+      <Text>
+        <b>Status:</b> {ticket.completed ? 'Completed' : 'Incomplete'}
+      </Text>
+    </Box>
   );
 }
 
--- client/src/app/tickets/tickets.tsx
+++ client/src/app/tickets/tickets.tsx
@@ -1,60 +1,63 @@
 import { Box } from '@welcome-ui/box';
+import { Button } from '@welcome-ui/button';
 import { Table } from '@welcome-ui/table';
 import { Text } from '@welcome-ui/text';
 
 import { Ticket } from '@acme/shared-models';
-import { Button } from '@welcome-ui/button';
 
 export interface TicketsProps {
   tickets: Ticket[];
 }
 
-export function Tickets(props: TicketsProps) {
+export function Tickets({ tickets }: TicketsProps) {
   return (
-    <Box
-      w={1}
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-    >
+    <Box w={1} display="flex" alignItems="center" justifyContent="center">
       <Box
+        flex={1}
+        maxW={960}
         display="flex"
+        flexDirection="column"
         alignItems="center"
         justifyContent="center"
-        spaceX="md"
       >
-        <Text variant="h2" color="primary-700">
-          Tickets
-        </Text>
+        <Box
+          w={1}
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Text variant="h2" color="primary-700">
+            Tickets
+          </Text>
 
-        <Button>Add new</Button>
+          <Button onClick={() => console.log('Clicked')}>Add new</Button>
+        </Box>
+        <Table>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>ID</Table.Th>
+              <Table.Th>Description</Table.Th>
+              <Table.Th>Assignee ID</Table.Th>
+              <Table.Th>Status</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+
+          {tickets ? (
+            <Table.Tbody>
+              {tickets.map(({ id, description, assigneeId, completed }) => (
+                <Table.Tr key={id}>
+                  <Table.Td>{id}</Table.Td>
+                  <Table.Td>{description}</Table.Td>
+                  <Table.Td>{assigneeId}</Table.Td>
+                  <Table.Td>{completed ? 'Completed' : 'Incomplete'}</Table.Td>
+                </Table.Tr>
+              ))}
+            </Table.Tbody>
+          ) : (
+            <Text>...</Text>
+          )}
+        </Table>
       </Box>
-      <Table maxW={960}>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>ID</Table.Th>
-            <Table.Th>Description</Table.Th>
-            <Table.Th>Assignee ID</Table.Th>
-            <Table.Th>Status</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-
-        {props.tickets ? (
-          <Table.Tbody>
-            {props.tickets.map(({ id, description, assigneeId, completed }) => (
-              <Table.Tr key={id}>
-                <Table.Td>{id}</Table.Td>
-                <Table.Td>{description}</Table.Td>
-                <Table.Td>{assigneeId}</Table.Td>
-                <Table.Td>{completed ? 'Completed' : 'Incomplete'}</Table.Td>
-              </Table.Tr>
-            ))}
-          </Table.Tbody>
-        ) : (
-          <Text>...</Text>
-        )}
-      </Table>
     </Box>
   );
 }
