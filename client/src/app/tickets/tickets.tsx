6124a1cacc6833879c68773de81f8a0d4b4b54d1
dat <vanthanhphuongdat1309@gmail.com>
2024-06-08 01:50:57 +0700

    filter tickets

--- client/src/app/tickets/tickets.tsx
+++ client/src/app/tickets/tickets.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { Box } from '@welcome-ui/box';
 import { Button } from '@welcome-ui/button';
 import { ArrowRightIcon } from '@welcome-ui/icons';
+import { OptionValue, Select } from '@welcome-ui/select';
 import { Table } from '@welcome-ui/table';
 import { Text } from '@welcome-ui/text';
 
@@ -12,6 +13,16 @@ import { Ticket } from '@acme/shared-models';
 export function Tickets() {
   const navigate = useNavigate();
   const [tickets, setTickets] = useState([] as Ticket[]);
+  const [filteredTickets, setFilteredTickets] = useState([] as Ticket[]);
+  const [filter, setFilter] = useState<OptionValue | OptionValue[]>();
+
+  const onSelectFilter = (value: OptionValue | OptionValue[]) => {
+    setFilter(value);
+  };
+
+  const onAddNewTicket = () => {
+    navigate('/add');
+  };
 
   useEffect(() => {
     async function fetchTickets() {
@@ -21,6 +32,28 @@ export function Tickets() {
 
     fetchTickets();
   }, []);
+
+  useEffect(() => {
+    if (tickets) {
+      setFilteredTickets(tickets);
+    }
+  }, [tickets]);
+
+  useEffect(() => {
+    if (filter) {
+      const filteredTickets = (function () {
+        if (filter === 'completed') {
+          return tickets.filter((t) => t.completed === true);
+        }
+
+        return tickets.filter((t) => t.completed === false);
+      })();
+
+      setFilteredTickets(filteredTickets);
+    } else {
+      setFilteredTickets(tickets);
+    }
+  }, [filter, tickets]);
 
   return (
     <Box w={1} display="flex" alignItems="center" justifyContent="center">
@@ -42,7 +75,29 @@ export function Tickets() {
             Tickets
           </Text>
 
-          <Button onClick={() => navigate('/add')}>Add new</Button>
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            spaceX="md"
+          >
+            <Box display="flex" alignItems="center" spaceX="md">
+              <Text>Filter by status:</Text>
+              <Select
+                options={[
+                  { label: 'Completed', value: 'completed' },
+                  { label: 'Incomplete', value: 'incomplete' },
+                ]}
+                name="welcome"
+                value={filter}
+                onChange={onSelectFilter}
+                placeholder="Select"
+                isClearable
+              />
+            </Box>
+
+            <Button onClick={onAddNewTicket}>Add new</Button>
+          </Box>
         </Box>
         <Table>
           <Table.Thead>
@@ -55,24 +110,28 @@ export function Tickets() {
             </Table.Tr>
           </Table.Thead>
 
-          {tickets ? (
+          {filteredTickets ? (
             <Table.Tbody>
-              {tickets.map(({ id, description, assigneeId, completed }) => (
-                <Table.Tr key={id}>
-                  <Table.Td>{id}</Table.Td>
-                  <Table.Td>{description}</Table.Td>
-                  <Table.Td>{assigneeId}</Table.Td>
-                  <Table.Td>{completed ? 'Completed' : 'Incomplete'}</Table.Td>
-                  <Table.Td textAlign="center">
-                    <ArrowRightIcon
-                      size="md"
-                      color={{ hover: 'primary-700' }}
-                      cursor="pointer"
-                      onClick={() => navigate(`/${id}`)}
-                    />
-                  </Table.Td>
-                </Table.Tr>
-              ))}
+              {filteredTickets.map(
+                ({ id, description, assigneeId, completed }) => (
+                  <Table.Tr key={id}>
+                    <Table.Td>{id}</Table.Td>
+                    <Table.Td>{description}</Table.Td>
+                    <Table.Td>{assigneeId}</Table.Td>
+                    <Table.Td>
+                      {completed ? 'Completed' : 'Incomplete'}
+                    </Table.Td>
+                    <Table.Td textAlign="center">
+                      <ArrowRightIcon
+                        size="md"
+                        color={{ hover: 'primary-700' }}
+                        cursor="pointer"
+                        onClick={() => navigate(`/${id}`)}
+                      />
+                    </Table.Td>
+                  </Table.Tr>
+                )
+              )}
             </Table.Tbody>
           ) : (
             <Text>...</Text>
